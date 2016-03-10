18b5781db60b643dc54a5c00c20d50c165825ffc
chenbojun <sysu_chenbojun@163.com>
2016-03-10 21:37:47 +0800

    Create the interface between view and presenter

---
+++ app/src/main/java/com/example/hzchenbojun/myapplication/view/IUserView.java
@@ -0,0 +1,11 @@
+package com.example.hzchenbojun.myapplication.view;
+
+import com.example.hzchenbojun.myapplication.bean.Book;
+
+/**
+ * Created by hzchenbojun on 2016/3/10.
+ */
+public interface IUserView {
+    String getBookid();
+    void displayBook(Book book);
+}
