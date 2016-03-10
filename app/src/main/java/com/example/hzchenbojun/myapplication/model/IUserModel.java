f0e58bba95252c428d9a610bffc844501b0f8e09
chenbojun <sysu_chenbojun@163.com>
2016-03-10 21:49:15 +0800

    Create the IUserMoldel to process data

---
+++ app/src/main/java/com/example/hzchenbojun/myapplication/model/IUserModel.java
@@ -0,0 +1,12 @@
+package com.example.hzchenbojun.myapplication.model;
+
+import com.example.hzchenbojun.myapplication.bean.Book;
+
+/**
+ * Created by hzchenbojun on 2016/3/10.
+ */
+public class IUserModel {
+    Book getBook(String id){
+        return null;
+    }
+}
